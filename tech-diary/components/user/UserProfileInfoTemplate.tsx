e39a26eca8a09cc91312e20ad7df9714422c691d
ohaeseong <74897099+ohaeseong@users.noreply.github.com>
2021-03-09 11:59:13 +0900

    [UPDATE] 블로그 게시글 작업 페이지 수정

---
+++ tech-diary/components/user/UserProfileInfoTemplate.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const UserProfileInfoWrap = styled.div`
+	width: 20rem;
+	height: 100%;
+	border: 1px solid black;
+`;
+
+function UserProfileInfoTemplate() {
+
+	return <UserProfileInfoWrap />;
+}
+
+export default UserProfileInfoTemplate;
